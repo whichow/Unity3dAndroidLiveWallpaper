296150df8fd29a3864f83abdbf81e9f6968f0815
wuhao <wuhao@orbbec.com>
2021-06-08 10:43:42 +0800

    Hide ui at preview and desktop

---
+++ Assets/UICtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICtrl : MonoBehaviour
+{
+    public GameObject ui;
+    private AndroidJavaClass appClass;
+
+    private string app = "ulw.ulw.ulw.App";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        appClass = new AndroidJavaClass(app);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnApplicationFocus(bool hasFocus)
+    {
+        bool active = appClass.GetStatic<bool>("ACT");
+        ui.SetActive(active);
+    }
+}
